17edbc4acada6ddc91cac23b1d485ac1f4613dfa
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2025-11-11 21:36:17 +0800

    feat: UI changes and added footer

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <section className="font-mono flex justify-center px-100 pt-30 text-sm text-gray-400">
+      <p>Developed by Aye Thida Tun.</p>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,31 @@ import RestartButton from "./_components/restart-button";
 import Results from "./_components/results";
 import useEngine from "./hooks/useEngine";
 import { calAccuracy, calwpm } from "./hooks/useCalculator";
-// import { timer } from "./hooks/useEngine";
 import { useState } from "react";
 import { Icon } from "@iconify/react/dist/iconify.js";
-// import Timers from "./_components/timers";
+import Footer from "./_components/footer";
 
 export default function HomePage() {
-  const [selectedTimer, setSelectedTimer] = useState(30);
+  const [selectedTimer, setSelectedTimer] = useState(10);
   const { state, words, typed, timeLeft, errors, restart, totalTyped } =
     useEngine(selectedTimer);
   return (
     <main className="">
       <p className="flex justify-center pt-20 pb-10 text-2xl font-mono text-yellow-950">
-        Coffee Type {""} 
-        <Icon icon="streamline-pixel:food-drink-coffee" width="32" height="32" className="ml-3"/>
+        Coffee Type {""}
+        <Icon
+          icon="streamline-pixel:food-drink-coffee"
+          width="32"
+          height="32"
+          className="ml-3"
+        />
       </p>
-      <section className="px-100 space-x-2">
+      <section className="px-100 pt-20 space-x-2">
         {[10, 30, 60].map((t) => (
           <button
             key={t}
             onClick={() => setSelectedTimer(t)}
-            className={`px-4 py-1 rounded-full font-mono text-sm border-0 ${
+            className={`px-4 py-1 rounded-full font-mono text-sm border-0 cursor-pointer ${
               selectedTimer === t
                 ? "bg-yellow-900 text-white"
                 : "bg-gray-200 text-gray-500"
@@ -46,6 +50,7 @@ export default function HomePage() {
         accuracy={calAccuracy(errors, totalTyped)}
         state={state}
       />
+      <Footer />
     </main>
   );
 }
