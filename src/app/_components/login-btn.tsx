c8ef3a067b53f83033511486566170f615aeaa1d
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2026-03-03 22:57:04 +0800

    feat: make login btn renderable for profile btn

--- src/app/_components/login-btn.tsx
+++ src/app/_components/login-btn.tsx
@@ -1,19 +1,23 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Icon } from "@iconify/react";
+import { useRouter } from "next/navigation";
 
-export default function LoginButton() {
+export default function LoginButton({ isLoggedIn }: { isLoggedIn: boolean }) {
+  const router = useRouter();
+
   return (
-    <section className="fixed flex justify-end top-3 right-5">
+    <div className="fixed flex justify-end top-3 right-5">
       <Button
         variant="outline"
         className="group p-5 cursor-pointer hover:bg-yellow-950 hover:text-white"
-        onClick={() => window.location.href = "/login"}
+        onClick={() => router.push(isLoggedIn ? "/profile" : "/login")}
       >
         <div className="flex items-center gap-2">
           <Icon icon="bxs:user" width="24" height="24" />
-          Login / Sign up
+          {isLoggedIn ? "Profile" : "Login / Sign up"}
         </div>
       </Button>
-    </section>
+    </div>
   );
 }
